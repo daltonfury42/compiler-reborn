32b4b242f64490f42b2b9a6ee7d1e9b133da541f
Dalton Fury <daltonfury42@disroot.org>
2019-01-10 15:33:01 +0000

    Front end - if and while

--- my-compiler/exptree.c
+++ my-compiler/exptree.c
@@ -4,50 +4,61 @@
 #include <string.h>
 #include "y.tab.h"
 
-tnode* createTree(int val, int type, char c, int operator, int nodetype, tnode* l, tnode* r )
+tnode* createTree(int val, int type, char varname, int operator, int nodetype, tnode* l, tnode* r, tnode* elseTree)
 {
 	tnode* temp = (tnode*)malloc(sizeof(tnode));
 	temp->val = val;
 	temp->type = type;
-	temp->varname = c;
+	temp->varname = varname;
 	temp->operator = operator;
 	temp->nodetype = nodetype;
 	temp->left=l;
 	temp->right=r;
+	temp->elseTree = elseTree;
 }
 
 tnode* makeConnectorNode(tnode* l, tnode* r)
 {
-	return createTree(-1, -1, -1, -1, CONNECTOR, l, r);
+	return createTree(-1, -1, -1, -1, CONNECTOR, l, r, NULL);
 }
 
 tnode* makeReadNode(tnode* lr)
 {
-	return createTree(-1, -1, -1, -1, READ, NULL, lr);
+	return createTree(-1, -1, -1, -1, READ, NULL, lr, NULL);
 }
 
 tnode* makeWriteNode(tnode* lr)
 {
-		return createTree(-1, -1, -1, -1, WRITE, NULL, lr);
+		return createTree(-1, -1, -1, -1, WRITE, NULL, lr, NULL);
 }
 
 tnode* makeLeafNodeVar(char ch)
 {
-	return createTree(-1, -1, ch, -1, VARIABLE, NULL, NULL);
+	return createTree(-1, -1, ch, -1, VARIABLE, NULL, NULL, NULL);
 }
 
 
 tnode* makeLeafNodeNum(int n)
 {
-	return createTree(n, -1, -1, -1, NUM, NULL, NULL);
+	return createTree(n, -1, -1, -1, NUM, NULL, NULL, NULL);
 }
 
 tnode* makeAssignmentNode(tnode* l, tnode* r)
 {
-	return createTree(-1, -1, -1, -1, ASGN, l, r);
+	return createTree(-1, -1, -1, -1, ASGN, l, r, NULL);
 }
 
 tnode* makeOperatorNode(int operator, tnode *l,tnode *r)
 {
-	return createTree(-1, -1, -1, operator, OPERATOR, l, r);
+	return createTree(-1, -1, -1, operator, OPERATOR, l, r, NULL);
+}
+
+tnode* makeIfNode(tnode* condition, tnode* ifTree, tnode* elseTree)
+{
+	return createTree(-1, -1, -1, -1, IF, condition, ifTree, elseTree);
+}
+
+tnode* makeWhileNode(tnode* condition, tnode* body) 
+{
+	return createTree(-1, -1, -1, -1, WHILE, condition, body, NULL);
 }
--- my-compiler/simpleCalculator.y
+++ my-compiler/simpleCalculator.y
@@ -12,10 +12,10 @@
 
 %}
 
-
 %error-verbose
 
-%token CONNECTOR READ WRITE VARIABLE ASGN BEG END NUM OPERATOR
+%token CONNECTOR READ WRITE VARIABLE ASGN BEG END NUM OPERATOR 
+%token IF THEN ELSE ENDIF WHILE DO ENDWHILE
 
 %left PLUS MINUS
 %left DIV MUL
@@ -49,6 +49,8 @@ slist 		: slist stmt 		{
 stmt 		: ReadStmt
 			| WriteStmt		
 			| AsgnStmt
+			| IfStmt
+			| WhileStmt
 			;
 
 ReadStmt 	: READ '(' VARIABLE ')' ';'		{ $$ = makeReadNode($3); }
@@ -57,14 +59,21 @@ WriteStmt 	: WRITE '(' expr ')' ';'		{ $$ = makeWriteNode($3); }
 
 AsgnStmt 	: VARIABLE ASGN expr ';'		{ $$ = makeAssignmentNode($1, $3); }
 
-expr	: expr PLUS expr		{ $$ = makeOperatorNode(PLUS, $1, $3); }
-	| expr MINUS expr			{ $$ = makeOperatorNode(MINUS, $1, $3); }
-	| expr MUL expr				{ $$ = makeOperatorNode(MUL, $1, $3); }
-	| expr DIV expr				{ $$ = makeOperatorNode(DIV, $1, $3); }
-	| '(' expr ')'	 			{ $$ = $2; }
-	| NUM						{ $$ = $1; }	// Node made in .l file
-	| VARIABLE						{ $$ = $1; }
-	;
+expr		: expr PLUS expr				{ $$ = makeOperatorNode(PLUS, $1, $3); }
+			| expr MINUS expr				{ $$ = makeOperatorNode(MINUS, $1, $3); }
+			| expr MUL expr					{ $$ = makeOperatorNode(MUL, $1, $3); }
+			| expr DIV expr					{ $$ = makeOperatorNode(DIV, $1, $3); }
+			| '(' expr ')'	 				{ $$ = $2; }
+			| NUM							{ $$ = $1; }	// Node made in .l file
+			| VARIABLE						{ $$ = $1; }
+			;
+
+IfStmt 		: IF '(' expr ')' THEN slist ELSE slist ENDIF 	{ $$ = makeIfNode($3, $6, $8); }
+			| IF '(' expr ')' THEN slist ENDIF				{ $$ = makeIfNode($3, $6, NULL); }
+			;
+
+WhileStmt 	: WHILE '(' expr ')' DO slist ENDWHILE			{ $$ = makeWhileNode($3, $6); }
+			;
 
 %%
 
